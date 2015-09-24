bd022d51d9504c845c90d2e2215b96cb988f424d
xingyue <qixingyue@126.com>
2015-09-24 18:25:48 +0800

    函数指针传递

--- a.c
+++ a.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+void helloworld(){
+	printf("this is hello world function \n");
+}
+
+void add_test_real(void (*fn_name) () ) {
+	fn_name();
+}
+
 int main(){
 	printf("Hello world... \n");
+	add_test_real(helloworld);
 	return 0;
 }
