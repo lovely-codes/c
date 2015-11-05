e34578b65bd8f2fa06080e9af707979a817aa55c
xingyue <qixingyue@126.com>
2015-11-05 22:12:43 +0800

    分配的内存空间

--- zmalloc.c
+++ zmalloc.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 #if defined(__APPLE__) 
 #include <malloc/malloc.h>
+#define PREFIX_SIZE 0
 #endif
 
 #ifdef __linux__
+#define PREFIX_SIZE sizeof(size_t)
 size_t malloc_size(void *ptr) {
 	return (size_t) *((size_t*)(ptr - PREFIX_SIZE));
 }
 #endif
 
-#define PREFIX_SIZE sizeof(size_t)
 
 static size_t memory_used = 0 ;
 
 void *zmalooc(size_t size) {
 	void *ptr = malloc(size + PREFIX_SIZE);
-	//mac系统下存在malloc_size函数
-	printf("REAL SIZE : %ld \n",malloc_size(ptr));
 	*((size_t *)ptr) = size + PREFIX_SIZE;
 	return ptr + PREFIX_SIZE;
 }
@@ -31,6 +31,8 @@ void zfree(void *ptr) {
 
 int main(int argc,char **argv){
 	char *str = (char*) zmalooc(sizeof(char) * 20);
+	//mac系统下存在malloc_size函数
+	printf("REAL SIZE : %ld \n",malloc_size(str));
 	strcpy(str,"Hello world\n");
 	printf("%s\n",str);
 	zfree(str);
