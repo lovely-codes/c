f83e1e2238426a683059556776dfcb688d064fa1
xingyue <qixingyue@126.com>
2015-11-20 17:07:05 +0800

    使用自己编写的测试框架

--- mytest.c
+++ mytest.c
@@ -1,29 +1,10 @@
 #include<stdio.h>
-
-/* The following lines make up our testing "framework" :) */
-static int tests = 0, fails = 0;
-#define test(_s) { printf("#%02d ", ++tests); printf("%s\n",_s); }
-#define test_cond(_c) if(_c) printf("\033[0;32mPASSED\033[0;0m\n"); else {printf("\033[0;31mFAILED\033[0;0m\n"); fails++;}
+#include "mytest.h"
 
 int main(){
-
 	int i=0;
-	test("Hello fail");
-	test_cond(0);
-	test("Hello ok");
-	test_cond(1);
-
-
-	printf("\n");
-	for(i=0;i<14;i++)printf("+ ");
-	printf("TEST RESULT");
-	for(i=0;i<14;i++)printf(" +");
-	printf("\n");
-	if( 0 == fails) {
-		printf("ALL PASSED\n");
-	} else {
-		printf("%d FAILED \n",fails);
-	}
-
+	test_cond("test a",1);
+	test_cond("test b",1);
+	test_report();
 	return 0;
 }
--- mytest.h
+++ mytest.h
@@ -1,6 +1,8 @@
 #ifndef __TESTHELP_H
 #define __TESTHELP_H
 
+#include <stdlib.h>
+
 #ifndef GREEN_OPEN 
 #define GREEN_OPEN "\033[0;32m"
 #endif
@@ -15,17 +17,19 @@
 
 int __failed_tests = 0;
 int __test_num = 0;
+
 #define test_cond(descr,_c) do { \
     __test_num++; printf("%d - %s: ", __test_num, descr); \
-    if(_c) printf( GREEN_OPEN "PASSED\n" COLOR_CLOSE ); else {printf( RED_OPEN "FAILED\n" COLOR_CLOSE ); __failed_tests++;} \
-} while(0);
+    if(_c) printf( GREEN_OPEN "PASSED\n" COLOR_CLOSE ); else {printf( RED_OPEN "FAILED %s : %d\n" COLOR_CLOSE , __FILE__ , __LINE__ ); __failed_tests++;} \
+} while(0) 
+
 #define test_report() do { \
-    printf("%d tests, %d passed, %d failed\n", __test_num, \
-                    __test_num-__failed_tests, __failed_tests); \
+    printf("\n%d tests, %d passed, %d failed\n", __test_num, \
+                    __test_num - __failed_tests, __failed_tests); \
     if (__failed_tests) { \
         printf("=== WARNING === We have failed tests here...\n"); \
         exit(1); \
     } \
-} while(0);
+} while(0)
 
 #endif
